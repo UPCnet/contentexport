b0cf0d3a29cc6bcacdfb5989ff51ec2ea9700f54
pmarinas <pilar.marinas@upcnet.es>
2023-02-22 09:11:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='contentexportgw4',
+    name='contentexport',
     version='0.1',
     description='Example for extending collective.exportimport',
-    url='https://github.com/UPCnet/contentexportgw4.git',
+    url='https://github.com/UPCnet/contentexport.git',
     author='Plone Team',
     author_email='ploneteam@upcnet.es',
     license='GPL version 2',
-    packages=['contentexportgw4'],
+    packages=['contentexport'],
     include_package_data=True,
     zip_safe=False,
     entry_points={'z3c.autoinclude.plugin': ['target = plone']},
