c789f3c259ab0c3ba873a9fecee2362608ee9c43
Pilar Marinas <pilar.marinas@upcnet.es>
2023-02-23 11:52:02 +0100

    Controlpanels

--- contentexport/export_controlpanels.py
+++ contentexport/export_controlpanels.py
@@ -6,7 +6,12 @@ from plone import api
 from plone.restapi.serializer.converters import json_compatible
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
-from genweb.controlpanel.interface import IGenwebControlPanelSettings 
+
+from genweb.controlpanel.interface import IGenwebControlPanelSettings
+from plone.formwidget.recaptcha.interfaces import IReCaptchaSettings
+
+import base64
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +38,13 @@ class ExportControlpanels(BaseExport):
         results["addons"] = addons
 
         portal = api.portal.get()
+
+        image_capcalera_url = portal.absolute_url() + '/portal_skins/custom/capcalera.jpg'
+        imgData = requests.get(image_capcalera_url).content
+        b64data_image_capcalera = str(base64.b64encode(imgData).decode('utf-8'))
         controlpanel = {}
+
+        #genweb-controlpanel
         gwsettings = getUtility(IRegistry).forInterface(IGenwebControlPanelSettings)
         controlpanel["genweb6.core.controlpanels.footer.IFooterSettings"]=dict(signatura_ca=gwsettings.signatura_unitat_ca,
                                                                                signatura_es=gwsettings.signatura_unitat_es,
@@ -43,6 +54,7 @@ class ExportControlpanels(BaseExport):
                                                                                html_title_ca=gwsettings.html_title_ca,
                                                                                html_title_es=gwsettings.html_title_es,
                                                                                html_title_en=gwsettings.html_title_en,
+                                                                               hero_image=b64data_image_capcalera,
                                                                                treu_menu_horitzontal=gwsettings.treu_menu_horitzontal,
                                                                                amaga_identificacio=gwsettings.amaga_identificacio,
                                                                                idiomes_publicats=gwsettings.idiomes_publicats,
@@ -52,5 +64,29 @@ class ExportControlpanels(BaseExport):
                                                                         contacte_BBDD_or_page=gwsettings.contacte_BBDD_or_page,
                                                                         contacte_multi_email=gwsettings.contacte_multi_email,
                                                                         contact_emails_table=gwsettings.contact_emails_table)
+        #@@mail-controlpanel
+        controlpanel["plone.app.controlpanel.mail.IMailSchema"]=dict(smtp_host=getattr(portal.MailHost, 'smtp_host', ''),
+                                                                     smtp_port=int(getattr(portal.MailHost, 'smtp_port', 25)),
+                                                                     smtp_userid=portal.MailHost.get('smtp_user_id'),
+                                                                     smtp_pass=portal.MailHost.get('smtp_pass'),
+                                                                     email_from_name=portal.getProperty('email_from_name', ''),
+                                                                     email_from_address=portal.getProperty('email_from_address', ''),
+                                                                    )
+        #@@site-controlpanel
+        portal_properties = api.portal.get_tool("portal_properties")
+        site_props = portal_properties.site_properties
+        controlpanel["plone.app.controlpanel.site.ISiteSchema"]=dict(site_title=portal.title,
+                                                                     site_description=portal.description,
+                                                                     exposeDCMetaTags=site_props.exposeDCMetaTags,
+                                                                     display_pub_date_in_byline=site_props.displayPublicationDateInByline,
+                                                                     enable_sitemap=site_props.enable_sitemap,
+                                                                     webstats_js=site_props.webstats_js)
+        #@@recaptcha-settings
+        recaptchasettings = getUtility(IRegistry).forInterface(IReCaptchaSettings)
+        controlpanel["plone.formwidget.recaptcha.interfaces.IReCaptchaSettings"]=dict(public_key=recaptchasettings.public_key,
+                                                                                      private_key=recaptchasettings.private_key,
+                                                                                      display_theme=recaptchasettings.display_theme,
+                                                                                      display_type=recaptchasettings.display_type,
+                                                                                      display_size=recaptchasettings.display_size)
         results["controlpanel"] = json_compatible(controlpanel)
         return results
